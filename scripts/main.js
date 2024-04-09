26e93cfd585df7df6c0be58585a60ba899339558
meiyichao <113711337+meiyichao@users.noreply.github.com>
2024-04-09 21:26:31 +0800

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector("img");
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/1.jpg") {
-    myImage.setAttribute("src", "images/2.jpg");
+  if (mySrc === "images/1.png") {
+    myImage.setAttribute("src", "images/2.png");
   } else {
-    myImage.setAttribute("src", "images/1.jpg");
+    myImage.setAttribute("src", "images/1.png");
   }
 };
 let myButton = document.querySelector("button");
@@ -14,7 +14,7 @@ let myHeading = document.querySelector("h1");
 function setUserName() {
     let myName = prompt("请输入你的名字。");
     localStorage.setItem("name", myName);
-    myHeading.textContent = "我爱" + myName;
+    myHeading.textContent = "最美" + myName;
 }
 
   
@@ -24,7 +24,7 @@ function setUserName() {
     setUserName();
   } else {
     let storedName = localStorage.getItem("name");
-    myHeading.textContent = "我爱" + storedName;
+    myHeading.textContent = "最美" + storedName;
   }
   
 
